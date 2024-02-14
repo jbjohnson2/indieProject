ded574a85ca8fd37ac75e1ef26026bf934ff90bb
jbjohnson2 <jbjohnson2@madisoncollege.edu>
2024-02-14 12:02:35 -0600

    add getters and setters to user entity

---
+++ src/main/java/entity/User.java
@@ -0,0 +1,175 @@
+package entity;
+import jakarta.persistence.*;
+import org.hibernate.annotations.GenericGenerator;
+
+/**
+ * The type User.
+ */
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO, generator="native")
+    @GenericGenerator(name = "native",strategy = "native")
+    private int id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "user_name")
+    private String userName;
+
+    @Column(name = "email_address")
+    private String emailAddress;
+
+    @Column(name = "zipcode")
+    private int zipcode;
+
+
+    /**
+     * Instantiates a new User.
+     */
+    public User() {
+    }
+
+    /**
+     * Instantiates a new User.
+     *
+     * @param firstName    the first name
+     * @param lastName     the last name
+     * @param userName     the user name
+     * @param emailAddress the email address
+     * @param zipcode      the zipcode
+     */
+    public User(String firstName, String lastName, String userName, String emailAddress, int zipcode) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.userName = userName;
+        this.emailAddress = emailAddress;
+        this.zipcode = zipcode;
+    }
+
+    /**
+     * Gets id.
+     *
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Sets id.
+     *
+     * @param id the id
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * Gets first name.
+     *
+     * @return the first name
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * Sets first name.
+     *
+     * @param firstName the first name
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     * Gets last name.
+     *
+     * @return the last name
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * Sets last name.
+     *
+     * @param lastName the last name
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    /**
+     * Gets user name.
+     *
+     * @return the user name
+     */
+    public String getUserName() {
+        return userName;
+    }
+
+    /**
+     * Sets user name.
+     *
+     * @param userName the user name
+     */
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    /**
+     * Gets email address.
+     *
+     * @return the email address
+     */
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    /**
+     * Sets email address.
+     *
+     * @param emailAddress the email address
+     */
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    /**
+     * Gets zipcode.
+     *
+     * @return the zipcode
+     */
+    public int getZipcode() {
+        return zipcode;
+    }
+
+    /**
+     * Sets zipcode.
+     *
+     * @param zipcode the zipcode
+     */
+    public void setZipcode(int zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", userName='" + userName + '\'' +
+                ", emailAddress='" + emailAddress + '\'' +
+                ", zipcode=" + zipcode +
+                '}';
+    }
+
+}
